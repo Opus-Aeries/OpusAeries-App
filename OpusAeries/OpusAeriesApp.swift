f1ec25e0b82b76a4fb7f0ad6188fa90ca9372cef
Ben Ostrovsky <ben.leo.ost@gmail.com>
2022-11-19 17:20:05 -0800

    Initial Commit

---
+++ OpusAeries/OpusAeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpusAeriesApp.swift
+//  OpusAeries
+//
+//  Created by Ben on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct OpusAeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
